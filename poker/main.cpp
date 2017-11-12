4d68f7c26a0252b5986532bbbad3994671a40bb5
vincentlau8 <vincentlau089@gmail.com>
2017-11-12 11:37:24 -0800

    Initial Commit

---
+++ poker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  poker
+//
+//  Created by Vincent Lau on 11/12/17.
+//  Copyright © 2017 vincenthayato. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
